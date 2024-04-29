c0de25eda2a844eed2bfacce9c389a04523dc001
Victor R Santos <vrsisa@hotmail.com>
2024-04-28 22:36:04 -0300

    alteração nome diretorio. resolvendo exercicios

---
+++ exercicios/1_capitulo/1_1.py
@@ -0,0 +1,12 @@
+# Exercicios
+
+# 1.1 - Suponha que você tenha uma lista com 128 nomes esteja fazendo uma pesquisa binária.
+# Qual seria o número máximo de etapas que você levaria para encontrar o nome desejado?
+
+## Resp: 7 etapas
+## Resolução: 128 -> 64 -> 32 -> 16 -> 8 -> 4 -> 2 -> 1
+
+# 1.2 - Suponha que você duplique o tamanho da lista. Qual seria o número máximo de etapas agora?
+
+## Resp: 8 etapas
+## Resolução: 256 -> 128 -> 64 -> 32 -> 16 -> 8 -> 4 -> 2 -> 1
